040cf8916f144dd3ccfb9c380091af75d8d9755e
Jonathan Hornung <jonathan.hornung@socialsweethearts.de>
2020-02-17 11:00:42 +0100

    added redux saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
+
 import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import promise from 'redux-promise';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
+import createSagaMiddleware from 'redux-saga'
 
+import { helloSaga } from '../sagas'
 import rootReducer from '../reducers';
 
 const persistConfig = {
@@ -12,13 +15,20 @@ const persistConfig = {
     blacklist: ['blogPage', 'blogFilter', 'casesPage', 'casesFilter'], // will not be persisted
 };
 
+
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleware = [promise];
+const middleware = [promise, sagaMiddleware];
 
 export const store = createStore(persistedReducer, composeWithDevTools(
     applyMiddleware(...middleware),
     // other store enhancers if any
 ));
+
+sagaMiddleware.run(helloSaga)
+
+export const action = type => store.dispatch({type})
 
 export const persistor = persistStore(store);
