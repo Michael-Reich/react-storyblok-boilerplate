048f1b2f9ffeba5a9f6debb40d86f3e553b487b3
Jonathan Hornung <jonathan.hornung@gmail.com>
2020-02-18 09:37:17 +0100

    improvements

--- src/components/modules/Modules/ModuleBlogTeaser.js
+++ src/components/modules/Modules/ModuleBlogTeaser.js
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import {connect} from 'react-redux';
 
+import {entityName} from '../../../entities/blog';
 import {fetchItems} from '../../../actions/blog';
 import {getMarginClasses} from '../../../tools/helper';
 import Item from '../../../scenes/Blog/Item';
@@ -20,23 +21,23 @@ const ModuleBlogTeaser = (props) => {
         }
     }, [props.module]);
 
-    const [blog, setBlog] = useState(props.blog);
+    const [items, setItems] = useState(props.items);
 
     const sortFn = (a, b) => new Date(b.content.date) - new Date(a.content.date);
 
     useEffect(() => {
-        if (props.blog && props.blog.length > 0) {
-            let tempBlog = [];
+        if (props.items && props.items.length > 0) {
+            let tempItems = [];
 
-            tempBlog = props.blog;
-            try{tempBlog.sort(sortFn);} catch(e){}
+            tempItems = props.items;
+            try{tempItems.sort(sortFn);} catch(e){}
 
             // if (module.content && module.content.sort === 'desc') {
-            //     tempBlog.reverse();
+            //     tempItems.reverse();
             // }
-            setBlog(tempBlog);
+            setItems(tempItems);
         }
-    }, [props.blog]);
+    }, [props.items]);
 
     useEffect(() => {
         props.fetchItems();
@@ -44,10 +45,10 @@ const ModuleBlogTeaser = (props) => {
 
     return <div className={`${marginClasses}`}>
         <Container fluid={module.is_full_width}>
-            {blog.length > 0 && <Row className={'h-100'}>
-                {[...blog].splice(0, 3).map((item, index) => {
+            {items.length > 0 && <Row className={'h-100'}>
+                {[...items].splice(0, 3).map((itm, index) => {
                     return <Col lg={4} md={6} key={index} style={{marginBottom: tools.margin,}}>
-                        <Item item={item}/>
+                        <Item item={itm}/>
                     </Col>;
                 })}
             </Row>}
@@ -58,12 +59,12 @@ const ModuleBlogTeaser = (props) => {
 
 ModuleBlogTeaser.defaultProps = {
     module: {},
-    blog: [],
+    items: [],
 };
 
 const mapStateToProps = (state) => {
     return {
-        blog: state.blog,
+        items: state[entityName].items,
     };
 };
 
